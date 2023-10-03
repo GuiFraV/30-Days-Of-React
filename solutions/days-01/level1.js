0553871f439d69ed44e5a6720136de062ff2953c
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-03 17:21:14 +0200

    chore: exercice level1 complete

---
+++ solutions/days-01/level1.js
@@ -0,0 +1,10 @@
+// Exercice Level 1:
+
+
+
+let emptyArray = [];
+let arrayOf5 = [1,2,3,4,5]
+console.log(arrayOf5.length)
+console.log(arrayOf5.slice(0,1))
+console.log(arrayOf5[arrayOf5.length -1])
+console.log(arrayOf5[(arrayOf5.length/2).toFixed() -1])
---
+++ solutions/days-01/main.js
@@ -0,0 +1,5 @@
+import { countries } from './countries.js';  // Assurez-vous que le chemin est correct
+import { webTechs } from './web_tech.js';   // Assurez-vous que le chemin est correct
+
+console.log(countries);
+console.log(webTechs);
